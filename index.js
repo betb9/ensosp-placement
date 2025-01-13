9bc5b0290e07de41837fe2cee78b40dc6da0778e
Etienne BENDER <bender.etienne57@gmail.com>
2025-01-13 21:27:19 +0100

    Add option to select within Paris placement or Department placement

--- index.js
+++ index.js
@@ -1,66 +1,104 @@
-const officialPlacementOrder = [
-	'Président de la République',
-	'Premier ministre',
-	'Président du Sénat',
-	'Président de l\'Assemblée Nationale',
-	'Anciens Présidents de la République',
-	'Gouvernement',
-	'Anciens Premiers ministres',
-	'Président du Conseil constitutionnel',
-	'Vice-président du Conseil d\'Etat',
-	'Président du Conseil économique, social et environnemental',
-	'Défenseur des droits',
-	'Députés',
-	'Sénateurs',
-	'Représentants au Parlement européen',
-	'Autorité judiciaire représentée par le premier président de la Cour de cassation et le procureur général près cette cour',
-	'Premier président de la Cour des comptes et le procureur général près cette cour',
-	'Grand chancelier de la Légion d\'honneur, chancelier de l\'ordre national du Mérite, et les membres des conseils de ces ordres',
-	'Chef d\'état-major des armées',
-	'Délégué national de l\'Ordre de la Libération',
-	'Préfet de la région d\'Ile-de-France, préfet de Paris',
-	'Préfet de police de Paris',
-	'Maire de Paris, président du conseil de Paris',
-	'Président du conseil régional d\'Ile-de-France',
-	'Chancelier de l\'Institut de France, les secrétaires perpétuels de l\'Académie française, de l\'Académie des inscriptions et belles-lettres, de l\'Académie des sciences, de l\'Académie des beaux-arts et de l\'Académie des sciences morales et politiques',
-	'Secrétaire général du Gouvernement, le secrétaire général de la défense et de la sécurité nationale et le secrétaire général du ministère des affaires étrangères',
-	'Président de la cour administrative d\'appel de Paris, le premier président de la cour d\'appel de Paris et le procureur général près cette cour',
-	'Délégué général pour l\'armement, le secrétaire général pour l\'administration du ministère de la défense, le chef d\'état-major de l\'armée de terre, le chef d\'état-major de la marine, le chef d\'état-major de l\'armée de l\'air et de l\'espace, le gouverneur militaire de Paris, commandant de la région terre Ile-de-France',
-	'Président de la Haute Autorité pour la transparence de la vie publique',
-	'Président du Conseil supérieur de l\'audiovisuel',
-	'Président de la Commission nationale de l\'informatique et des libertés',
-	'Président de l\'Autorité de la concurrence',
-	'Président de l\'Autorité des marchés financiers',
-	'Recteur de l\'académie de Paris, chancelier des universités',
-	'Hauts-commissaires, commissaires généraux, commissaires, délégués généraux, délégués, secrétaires généraux, directeurs de cabinet, le directeur général de la gendarmerie nationale, les directeurs généraux et directeurs d\'administration centrale',
-	'Gouverneur de la Banque de France, le directeur général de la Caisse des dépôts et consignations',
-	'Président du tribunal administratif de Paris, le président du tribunal judiciaire de Paris et le procureur de la République près ce tribunal, le président de la chambre régionale des comptes d\'Ile-de-France',
-	'Préfet, secrétaire général aux politiques publiques de la préfecture de la région d\'Ile-de-France, préfecture de Paris ; le préfet, directeur du cabinet du préfet de police ; le préfet, secrétaire général aux moyens mutualisés de la préfecture de la région d\'Ile-de-France, préfecture de Paris ; le préfet, directeur du cabinet du préfet de la région d\'Ile-de-France, préfet de Paris ; le préfet, secrétaire général pour l\'administration ; le préfet, secrétaire général de la zone de défense et de sécurité de Paris ; le préfet délégué à l\'immigration ; le préfet délégué pour la sécurité et la sûreté des plates-formes de Paris-Charles de Gaulle, du Bourget et de Paris-Orly',
-	'Membres du conseil de Paris, les membres du conseil régional d\'Ile-de-France',
-	'Chef du contrôle général des armées, les généraux de division ayant rang et appellation de généraux d\'armée, les vices-amiraux ayant rang et appellation d\'amiraux, les généraux de division aérienne ayant rang et appellation de généraux d\'armée aérienne, les généraux de division ayant rang et appellation de généraux de corps d\'armée, les vice-amiraux ayant rang et appellation de vice-amiraux d\'escadre, les généraux de division aérienne ayant rang et appellation de généraux de corps aérien',
-	'Présidents des universités de Paris, les directeurs des grandes écoles nationales, les directeurs des grands établissements nationaux de recherche',
-	'Président du tribunal de commerce de Paris',
-	'Président du conseil de prud\'hommes de Paris',
-	'Secrétaire général de la ville de Paris',
-	'Directeur général des services administratifs de la région d\'Ile-de-France',
-	'Présidents et secrétaires perpétuels des académies créées ou reconnues par une loi ou un décret',
-	'Président du Conseil économique, social et environnemental de la région d\'Ile-de-France',
-	'Chefs des services déconcentrés de l\'Etat dans la région d\'Ile-de-France et dans le département de Paris dans l\'ordre de préséance attribué au département ministériel dont ils relèvent et les directeurs généraux et directeurs de la préfecture de région, de la préfecture de la région d\'Ile-de-France, préfecture de Paris et de la préfecture de police',
-	'Président de CCI France, le président de Chambres d\'agriculture France, le président de CMA France',
-	'Président de la chambre de commerce et d\'industrie de Paris, le président de la chambre régionale de commerce et d\'industrie d\'Ile-de-France',
-	'Président de la chambre régionale d\'agriculture d\'Ile-de-France, le président de la chambre interdépartementale d\'agriculture d\'Ile-de-France',
-	'Président de la chambre départementale de métiers de Paris',
-	'Président du conseil de l\'ordre des avocats au Conseil d\'Etat et à la Cour de cassation',
-	'Bâtonnier de l\'ordre des avocats au barreau de Paris et le président de la conférence des bâtonniers',
-	'Présidents des conseils nationaux des ordres professionnels',
-	'Directeurs des services de la ville de Paris dans l\'ordre de leur nomination',
-	'Commissaires de police, les officiers de gendarmerie et les officiers de la brigade de sapeurs-pompiers de Paris',
-	'Président de la Chambre nationale des avoués près les cours d\'appel',
-	'Président du Conseil supérieur du notariat',
-	'Président de la Chambre nationale des commissaires-priseurs judiciaires',
-	'Président de la Chambre nationale des huissiers de justice',
-	'Président de la Compagnie nationale des commissaires aux comptes'
+const parisPlacementOrder = [
+	"Le Président de la République",
+	"Le Premier ministre",
+	"Le président du Sénat",
+	"Le président de l'Assemblée nationale",
+	"Les anciens présidents de la République dans l'ordre de préséance déterminé par l'ancienneté de leur prise de fonctions",
+	"Le Gouvernement dans l'ordre de préséance arrêté par le Président de la République",
+	"Les anciens premiers ministres dans l'ordre de préséance déterminé par l'ancienneté de leur prise de fonctions",
+	"Le président du Conseil constitutionnel",
+	"Le vice-président du Conseil d'Etat",
+	"Le président du Conseil économique, social et environnemental",
+	"Le Défenseur des droits",
+	"Les députés",
+	"Les sénateurs",
+	"Les représentants au Parlement européen",
+	"L'autorité judiciaire représentée par le premier président de la Cour de cassation et le procureur général près cette cour",
+	"Le premier président de la Cour des comptes et le procureur général près cette cour",
+	"Le grand chancelier de la Légion d'honneur, chancelier de l'ordre national du Mérite, et les membres des conseils de ces ordres",
+	"Le chef d'état-major des armées",
+	"Le délégué national de l'Ordre de la Libération",
+	"Le préfet de la région d'Ile-de-France, préfet de Paris",
+	"Le préfet de police de Paris",
+	"Le maire de Paris, président du conseil de Paris",
+	"Le président du conseil régional d'Ile-de-France",
+	"Le chancelier de l'Institut de France, les secrétaires perpétuels de l'Académie française, de l'Académie des inscriptions et belles-lettres, de l'Académie des sciences, de l'Académie des beaux-arts et de l'Académie des sciences morales et politiques",
+	"Le secrétaire général du Gouvernement, le secrétaire général de la défense et de la sécurité nationale et le secrétaire général du ministère des affaires étrangères",
+	"Le président de la cour administrative d'appel de Paris, le premier président de la cour d'appel de Paris et le procureur général près cette cour",
+	"Le délégué général pour l'armement, le secrétaire général pour l'administration du ministère de la défense, le chef d'état-major de l'armée de terre, le chef d'état-major de la marine, le chef d'état-major de l'armée de l'air et de l'espace, le gouverneur militaire de Paris, commandant de la région terre Ile-de-France",
+	"Le président de la Haute Autorité pour la transparence de la vie publique",
+	"Le président du Conseil supérieur de l'audiovisuel",
+	"Le président de la Commission nationale de l'informatique et des libertés",
+	"Le président de l'Autorité de la concurrence",
+	"Le président de l'Autorité des marchés financiers",
+	"Le recteur de l'académie de Paris, chancelier des universités",
+	"Les hauts-commissaires, commissaires généraux, commissaires, délégués généraux, délégués, secrétaires généraux, directeurs de cabinet, le directeur général de la gendarmerie nationale, les directeurs généraux et directeurs d'administration centrale dans l'ordre de préséance des ministères déterminé par l'ordre protocolaire du Gouvernement et, au sein de chaque ministère, dans l'ordre de préséance déterminé par leur fonction ou leur grade",
+	"Le gouverneur de la Banque de France, le directeur général de la Caisse des dépôts et consignations",
+	"Le président du tribunal administratif de Paris, le président du tribunal judiciaire de Paris et le procureur de la République près ce tribunal, le président de la chambre régionale des comptes d'Ile-de-France",
+	"Le préfet, secrétaire général aux politiques publiques de la préfecture de la région d'Ile-de-France, préfecture de Paris ; le préfet, directeur du cabinet du préfet de police ; le préfet, secrétaire général aux moyens mutualisés de la préfecture de la région d'Ile-de-France, préfecture de Paris ; le préfet, directeur du cabinet du préfet de la région d'Ile-de-France, préfet de Paris ; le préfet, secrétaire général pour l'administration ; le préfet, secrétaire général de la zone de défense et de sécurité de Paris ; le préfet délégué à l'immigration ; le préfet délégué pour la sécurité et la sûreté des plates-formes de Paris-Charles de Gaulle, du Bourget et de Paris-Orly",
+	"Les membres du conseil de Paris, les membres du conseil régional d'Ile-de-France",
+	"Le chef du contrôle général des armées, les généraux de division ayant rang et appellation de généraux d'armée, les vices-amiraux ayant rang et appellation d'amiraux, les généraux de division aérienne ayant rang et appellation de généraux d'armée aérienne, les généraux de division ayant rang et appellation de généraux de corps d'armée, les vice-amiraux ayant rang et appellation de vice-amiraux d'escadre, les généraux de division aérienne ayant rang et appellation de généraux de corps aérien",
+	"Les présidents des universités de Paris, les directeurs des grandes écoles nationales, les directeurs des grands établissements nationaux de recherche",
+	"Le président du tribunal de commerce de Paris",
+	"Le président du conseil de prud'hommes de Paris",
+	"Le secrétaire général de la ville de Paris",
+	"Le directeur général des services administratifs de la région d'Ile-de-France",
+	"Les présidents et secrétaires perpétuels des académies créées ou reconnues par une loi ou un décret",
+	"Le président du Conseil économique, social et environnemental de la région d'Ile-de-France",
+	"Les chefs des services déconcentrés de l'Etat dans la région d'Ile-de-France et dans le département de Paris dans l'ordre de préséance attribué au département ministériel dont ils relèvent et les directeurs généraux et directeurs de la préfecture de région, de la préfecture de la région d'Ile-de-France, préfecture de Paris et de la préfecture de police",
+	"Le président de CCI France, le président de Chambres d'agriculture France, le président de CMA France",
+	"Le président de la chambre de commerce et d'industrie de Paris, le président de la chambre régionale de commerce et d'industrie d'Ile-de-France",
+	"Le président de la chambre régionale d'agriculture d'Ile-de-France, le président de la chambre interdépartementale d'agriculture d'Ile-de-France",
+	"Le président de la chambre départementale de métiers de Paris",
+	"Le président du conseil de l'ordre des avocats au Conseil d'Etat et à la Cour de cassation",
+	"Le bâtonnier de l'ordre des avocats au barreau de Paris et le président de la conférence des bâtonniers",
+	"Les présidents des conseils nationaux des ordres professionnels",
+	"Les directeurs des services de la ville de Paris dans l'ordre de leur nomination",
+	"Les commissaires de police, les officiers de gendarmerie et les officiers de la brigade de sapeurs-pompiers de Paris",
+	"Le président de la Chambre nationale des avoués près les cours d'appel",
+	"Le président du Conseil supérieur du notariat",
+	"Le président de la Chambre nationale des commissaires-priseurs judiciaires",
+	"Le président de la Chambre nationale des huissiers de justice",
+	"Le président de la Compagnie nationale des commissaires aux comptes"
 ];
+const departmentPlacementOrder = [
+	"Le préfet, représentant de l'État dans le département ou la collectivité",
+	"Les députés",
+	"Les sénateurs",
+	"Les représentants au Parlement européen élus en France",
+	"Le président du conseil régional",
+	"Le président du conseil départemental",
+	"Le maire de la commune où se déroule la cérémonie",
+	"Le président de l'établissement public de coopération intercommunale à fiscalité propre dont est membre la commune, lorsque la cérémonie se déroule dans une commune membre",
+	"Les conseillers régionaux",
+	"Les conseillers départementaux",
+	"Les maires des autres communes du département",
+	"Les adjoints au maire de la commune où se déroule la cérémonie",
+	"Les conseillers municipaux de la commune où se déroule la cérémonie",
+	"Le président du tribunal judiciaire",
+	"Le procureur de la République près ledit tribunal",
+	"Le président du tribunal administratif",
+	"Le recteur d'académie",
+	"Le vice-recteur",
+	"Le directeur académique des services de l'éducation nationale agissant sur délégation du recteur d'académie",
+	"Le directeur des services de greffe judiciaires du tribunal judiciaire",
+	"Le président du conseil économique, social et environnemental régional",
+	"Le président de la chambre de commerce et d'industrie territoriale",
+	"Le président de la chambre de métiers et de l'artisanat de région",
+	"Le président de la chambre d'agriculture",
+	"Le bâtonnier de l'ordre des avocats",
+	"Le président de l'université",
+	"Le directeur du centre hospitalier régional ou du centre hospitalier de la commune",
+	"Les officiers généraux et les officiers commandant les forces armées dans le département",
+	"Les chefs de service des administrations civiles de l'État dans le département",
+	"Les présidents des associations d'anciens combattants et victimes de guerre",
+	"Les représentants des cultes"
+];
+const placementOrders = {
+	parisPlacementOrder,
+	departmentPlacementOrder
+};
+let currentLocationOrderType = document.getElementById('placementLocation').value;
 
 printInputs();
 document.getElementById('generateButton').addEventListener('click', () => {
@@ -69,12 +107,20 @@ document.getElementById('generateButton').addEventListener('click', () => {
 document.getElementById('availablePlacement').addEventListener('input', () => {
 	renderPlacements()
 });
+document.getElementById('placementLocation').addEventListener('input', event => {
+	currentLocationOrderType = event.currentTarget.value;
+	printInputs();
+});
 
 function printInputs() {
 	const inputTable = document.getElementById('peopleInputs');
+	inputTable.innerHTML = '';
+
 	const tbody = document.createElement('tbody');
 
-	officialPlacementOrder.forEach((value, index) => {
+	const placementOrderList = getPlacementOrderList();
+
+	placementOrderList.forEach((value, index) => {
 		const row = document.createElement('tr');
 
 		const labelColumn = document.createElement('td');
@@ -138,4 +184,8 @@ function getPresentPeoples() {
 	});
 
 	return presentPeoples;
+}
+function getPlacementOrderList() {
+	const placementName = `${currentLocationOrderType}PlacementOrder`;
+	return placementOrders[placementName];
 }
